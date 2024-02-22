36a66b0832dae4c021192688e72ea4e83dbf8d08
duducg <eduardo.sang.rocha@gmail.com>
2024-02-22 22:56:17 +0000

    integer types

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //integer type variables
+            int i = -4;
+            uint ui = 4U;
+            sbyte sb = -128;
+            short s = -1000;
+            ushort us = 1000;             
+            long l = -5L;
+            ulong ul = 5UL;
+            byte bt = 16;
+
+            char a = 'a';
+            char a_Unicode = '\u0061';
+            
+            //Print values:
+            Console.WriteLine(i);
+            Console.WriteLine(ui);
+            Console.WriteLine(sb);
+            Console.WriteLine(s);
+            Console.WriteLine(us);
+            Console.WriteLine(l);
+            Console.WriteLine(ul);
+            Console.WriteLine(bt);
+            Console.WriteLine(a);
+            Console.WriteLine(a_Unicode);
+
+
+        }
+    }
+}
