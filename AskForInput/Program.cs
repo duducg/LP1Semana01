3ae931998f01d4bc3558e4c17c15fc91d60dbc4c
duducg <eduardo.sang.rocha@gmail.com>
2024-02-23 01:29:48 +0000

    ask for input and parse to int

---
+++ AskForInput/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AskForInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get user input and parse to int:
+            Console.WriteLine("Please write a integer number:");
+            int userin_int = int.Parse(Console.ReadLine());            
+            
+
+        }
+    }
+}
