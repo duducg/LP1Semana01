bf1785ee98e6f7b8804df713bcff6533a88c94d4
duducg <eduardo.sang.rocha@gmail.com>
2024-02-23 01:36:59 +0000

    add both variables and print to console

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -9,11 +9,13 @@ namespace AskForInput
             //Get user input and parse to int:
             Console.WriteLine("Please write a integer number:");
             int userin_int = int.Parse(Console.ReadLine());   
-            
+
             //Parse to float:
             Console.WriteLine("Please write a real number:");
             float userin_float = float.Parse((Console.ReadLine()));         
             
+            //Add the values together:
+            float userint_sum = userin_int + userin_float;
 
         }
     }
